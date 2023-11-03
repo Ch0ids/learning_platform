bd1bf145799a6d87a65df5a8f1b4629e96ad2191
Choids <hiep.bh.02@gmail.com>
2023-11-04 03:31:25 +0700

    Add project files.

---
+++ WTFtest/Program.cs
@@ -0,0 +1,15 @@
+﻿var x = "abc";
+Console.WriteLine($"Hello, World! {x}");
+void tryCatch()
+{
+    try
+    {
+        Console.WriteLine("abcccc");
+        throw new Exception($"{x}");  
+    }
+    catch(Exception e)
+    {
+        throw new Exception(e.Message);
+    }
+}
+tryCatch();
